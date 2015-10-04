0a1f58e8404ee5383dba4f7dc45c8317068697c8
Elizabeth Hale <lizzhale@mac.com>
2015-10-04 11:09:30 -0700

    stream-adventure lesson 3

--- program.js
+++ program.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
 
-// pipes the contents of the first argument to this programs to stdout
-fs.createReadStream(process.argv[2]).pipe(process.stdout);
+
+// pipes the contents of stdin to stdout
+process.stdin.pipe(process.stdout);
