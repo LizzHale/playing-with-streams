e7857e7fe581052fed92c6918ef0398577c3f1ba
Elizabeth Hale <lizzhale@mac.com>
2015-10-04 11:36:04 -0700

    stream-adventure lesson 4

--- program.js
+++ program.js
@@ -1,5 +1,24 @@
 var fs = require('fs');
+var through = require('through2');
+
+// write and end are both optional
+// If write is not specified, the default implementation passes the input data to the ouput unmodified
+// If end is not specified, the default implementation calls 'this.push(null)' to close the output side when the input side ends
+var stream = through(write, end);
+
+// The write function is called for every buffer of available input
+function write (buffer, encoding, next) {
+    this.push(buffer.toString().toUpperCase());
+    next();
+}
+
+// This is the function called when there is no more data
+function end (done) {
+    done();
+}
+
+
 
 
 // pipes the contents of stdin to stdout
-process.stdin.pipe(process.stdout);
+process.stdin.pipe(stream).pipe(process.stdout);
