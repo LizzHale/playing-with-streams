2ff262b85026f88d311545fb8c48412f8ddc2c3b
Elizabeth Hale <lizzhale@mac.com>
2015-10-04 14:52:36 -0700

    stream-adventure lesson 6

--- program.js
+++ program.js
@@ -1,36 +1,15 @@
 var fs = require('fs');
-var through = require('through2');
-var split = require('split');
+var concat = require('concat-stream');
 
-var count = 1;
-
-// write and end are both optional
-// If write is not specified, the default implementation passes the input data to the ouput unmodified
-// If end is not specified, the default implementation calls 'this.push(null)' to close the output side when the input side ends
-var stream = through(write, end);
+var stream = concat(write);
 
 // The write function is called for every buffer of available input
-function write (buffer, encoding, next) {
-    if (count % 2) {
-        this.push('\n' + buffer.toString().toLowerCase());
-        count++;
-        next();
-    } else{
-        this.push('\n' + buffer.toString().toUpperCase(0));
-        count++;
-        next();
-    }
+function write(data) {
+    process.stdout.write(data.toString().split('').reverse().join(''));
+
 };
 
-// This is the function called when there is no more data
-function end (done) {
-    done();
-}
+// We cannot pipe the results of the concat stream to stdout because concat is asynchronous
+// Instead we write to standard out inside the concat callback function
+process.stdin.pipe(stream);
 
-process.stdin
-    .pipe(split())
-    .pipe(stream)
-    .pipe(process.stdout);
-
-// pipes the contents of stdin to stdout
-//process.stdin.pipe(stream).pipe(process.stdout);
