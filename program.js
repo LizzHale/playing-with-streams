3c5a05419b103d5cdeebce002088222e072fb1b0
Elizabeth Hale <lizzhale@mac.com>
2015-10-04 11:04:59 -0700

    stream-adventure lesson 2

--- program.js
+++ program.js
@@ -1,1 +1,4 @@
-console.log('beep boop');
+var fs = require('fs');
+
+// pipes the contents of the first argument to this programs to stdout
+fs.createReadStream(process.argv[2]).pipe(process.stdout);
