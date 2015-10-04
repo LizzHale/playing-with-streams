cbceb4695c7332ea8761dfc305218fc720993bfe
Elizabeth Hale <lizzhale@mac.com>
2015-10-04 13:30:00 -0700

    stream-adventure lesson 5

--- program.js
+++ program.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
 var through = require('through2');
+var split = require('split');
+
+var count = 1;
 
 // write and end are both optional
 // If write is not specified, the default implementation passes the input data to the ouput unmodified
@@ -8,17 +11,26 @@ var stream = through(write, end);
 
 // The write function is called for every buffer of available input
 function write (buffer, encoding, next) {
-    this.push(buffer.toString().toUpperCase());
-    next();
-}
+    if (count % 2) {
+        this.push('\n' + buffer.toString().toLowerCase());
+        count++;
+        next();
+    } else{
+        this.push('\n' + buffer.toString().toUpperCase(0));
+        count++;
+        next();
+    }
+};
 
 // This is the function called when there is no more data
 function end (done) {
     done();
 }
 
-
-
+process.stdin
+    .pipe(split())
+    .pipe(stream)
+    .pipe(process.stdout);
 
 // pipes the contents of stdin to stdout
-process.stdin.pipe(stream).pipe(process.stdout);
+//process.stdin.pipe(stream).pipe(process.stdout);
