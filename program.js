ef91f4cce83b41019ec0d42d975e582f2bbad242
Elizabeth Hale <lizzhale@mac.com>
2015-10-04 18:21:14 -0700

    stream-adventure lesson 7

--- program.js
+++ program.js
@@ -1,15 +1,30 @@
-var fs = require('fs');
-var concat = require('concat-stream');
+var http = require('http');
+var through = require('through2');
 
-var stream = concat(write);
+var server = http.createServer(function(req, res) {
+    if (req.method != 'POST') return res.end('send me a POST\n');
 
-// The write function is called for every buffer of available input
-function write(data) {
-    process.stdout.write(data.toString().split('').reverse().join(''));
+    var tr = through(function(chunk, encoding, next){
+        this.push(chunk.toString().toUpperCase());
+        next();
+    });
 
-};
+    req.pipe(tr).pipe(res)
+});
 
-// We cannot pipe the results of the concat stream to stdout because concat is asynchronous
-// Instead we write to standard out inside the concat callback function
-process.stdin.pipe(stream);
+server.listen(process.argv[2]);
+
+//var concat = require('concat-stream');
+//
+//var stream = concat(write);
+//
+//// The write function is called for every buffer of available input
+//function write(data) {
+//    process.stdout.write(data.toString().split('').reverse().join(''));
+//
+//};
+//
+//// We cannot pipe the results of the concat stream to stdout because concat is asynchronous
+//// Instead we write to standard out inside the concat callback function
+//process.stdin.pipe(stream);
 
